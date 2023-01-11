5ce778600eef0e16e76b4caebdb2a3036b445e5e
ryuk04 <122408850+ryuk04@users.noreply.github.com>
2023-01-11 21:39:24 +0530

    Update start.py

--- src/source/start.py
+++ src/source/start.py
@@ -1,5 +1,6 @@
 import os
 import re
+import random
 from platform import python_version as kontol
 from telethon import events, Button
 from telegram import __version__ as telever
@@ -8,11 +9,15 @@ from pyrogram import __version__ as pyrover
 from src.events import register
 from src import telethn as tbot
 
-VENOM = "https://telegra.ph/file/e3cd9302d6c371593c50d.mp4"
+VENOM = (
+      "https://graph.org/file/0cee276f2baecdf7df0b9.jpg",
+      "https://graph.org/file/79666d9264f849fb06a50.jpg",
+)
 
 @register(pattern=("/start"))
 async def awake(event):
-  TEXT = f"Heya [{event.sender.first_name}](tg://user?id={event.sender.id}), My name is Miku - I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.\n\n"
-  TEXT += "Join my [News Channel](https://t.me/MikuBotUpdates) to get information on all the latest updates."
-  BUTTON = [[Button.url("➕Add Me To Your Group➕", "https://t.me/mikuprorobot?startgroup=true"),]]
-  await tbot.send_file(event.chat_id, VENOM, caption=TEXT, buttons=BUTTON)
+  TEXT = f"Hey [{event.sender.first_name}](tg://user?id={event.sender.id}), Myself Raiden Shogun- I'm here to help you manage your groups with advance and lots of features!\n\n"
+  TEXT += "**Hit /help to find out more about how to use me to my full potential.**"
+  BUTTON = [[Button.url("➕Add Me To Your Chat➕", "https://t.me/mikuprorobot?startgroup=true"),]]
+  BUTTON+= [[Button.url("Help", "https://t.me/mikuprorobot?startgroup=true"),]]
+  await tbot.send_file(event.chat_id, random.choice(VENOM), caption=TEXT, buttons=BUTTON)
